5d84aec24922096aee8614b60784855cb6f46521
Tanner <tanner.crockett@gmail.com>
2019-06-06 21:11:23 -0600

    config firebase

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: "https://authentesizer.firebaseio.com",
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGE_SENDER_ID,
+    appId: process.env.APP_ID
+  };
